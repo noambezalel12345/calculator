c2e7fb608fe2b9434b1c260cf84b84003c5acb7c
noambezalel12345 <noambezalel2008@gmail.com>
2024-11-28 09:43:30 +0200

    version1

---
+++ app/src/main/java/com/example/simpleclaculater1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.simpleclaculater1;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Num1), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onBtnClicked(View view) {
+    }
+}
